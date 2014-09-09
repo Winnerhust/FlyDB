383be828dccc1b27f4cfa6904776ffb05cd44168
Winnerhust <910894904@qq.com>
2014-09-09 15:26:04 +0800

    Create file_test.cpp

---
+++ test/file_test.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include <string.h>
+#include "file.h"
+
+using namespace std;
+using namespace flydb;
+
+TEST(File,open)
+{
+	File f;
+
+	EXPECT_EQ(f.open("test_open.txt"),0);
+}
+TEST(File,addpage)
+{
+	File f;
+
+	EXPECT_EQ(f.open("test_open.txt"),0);
+	
+	//
+	char pagedata[PAGE_SIZE]="";
+	memset(pagedata,0,PAGE_SIZE);
+
+	*(int *)&pagedata[PAGE_SIZE-sizeof(int)*2] = 0;
+	*(int *)&pagedata[PAGE_SIZE-sizeof(int)] = 0;
+
+	Page page;
+	page.frombinary(pagedata);
+
+	EXPECT_EQ(f.get_page_num(),0);
+
+	f.addpage(page,0);
+	
+	EXPECT_EQ(f.get_page_num(),1);
+
+	f.addpage(page,0);
+
+	EXPECT_EQ(f.get_page_num(),1);
+	
+	f.addpage(page,1);
+
+	EXPECT_EQ(f.get_page_num(),2);
+	
+	f.addpage(page,3);
+
+	EXPECT_EQ(f.get_page_num(),4);
+
+}
+
+TEST(File,getpage)
+{
+	File f;
+
+	EXPECT_EQ(f.open("test_open_get.txt"),0);
+	
+	//
+	char pagedata[PAGE_SIZE]="";
+	memset(pagedata,0,PAGE_SIZE);
+
+	*(int *)&pagedata[PAGE_SIZE-sizeof(int)*2] = 2;
+	*(int *)&pagedata[PAGE_SIZE-sizeof(int)] = 0;
+
+	Page page;
+	page.frombinary(pagedata);
+
+
+	f.addpage(page,0);
+	f.addpage(page,1);
+	f.addpage(page,3);
+
+	EXPECT_EQ(f.get_page_num(),4);
+
+	Page t;
+
+	f.getpage(t,0);
+
+	char tdata[PAGE_SIZE];
+	memset(tdata,0x33,PAGE_SIZE);
+	t.tobinary(tdata);
+	EXPECT_EQ( *(int *)&pagedata[PAGE_SIZE-sizeof(int)*2],2);
+	EXPECT_EQ( *(int *)&pagedata[PAGE_SIZE-sizeof(int)],0);
+	
+	f.getpage(t,1);
+	t.tobinary(tdata);
+	EXPECT_EQ( *(int *)&pagedata[PAGE_SIZE-sizeof(int)*2],2);
+	EXPECT_EQ( *(int *)&pagedata[PAGE_SIZE-sizeof(int)],0);
+	
+	f.getpage(t,2);
+	t.tobinary(tdata);
+	EXPECT_EQ( *(int *)&pagedata[PAGE_SIZE-sizeof(int)*2],2);
+	EXPECT_EQ( *(int *)&pagedata[PAGE_SIZE-sizeof(int)],0);
+
+}
+
+#ifndef GTEST_MAIN
+#define GTEST_MAIN
+
+int main(int argc, char* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
+#endif
